7733265686d2b16028b9a9de1e1617c032d08f24
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-07-04 17:50:24 +0900

    Update change.js

--- change.js
+++ change.js
@@ -1,17 +1,18 @@
-function getFileName()
-{
-	return window.location.href.split('/').pop();
+function getFileName(){
+  return window.location.href.split('/').pop();
 }
-
 var filename = getFileName();
 var opt;
-if(filename === 'index.html')
-	{opt=document.querySelector('option[value = "index.html"]');}
-else
-	{opt=document.querySelector('option[value = "nothing.html"]');}
-opt.selected = true;
-
-document.getElementById('form').select.onchange = function()
+if(filename === 'nothing.html')
 {
-	location.href=document.getElementById('form').select.value;
+  opt = document.querySelector('option[value="nothing.html"]');
+}
+else
+{
+ opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
+
+document.getElementById('form').select.onchange=function(){
+  location.href = document.getElementById('form').select.value
 }
