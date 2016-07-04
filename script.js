e1ac989d8c287c26a96890ad1151432cac68e980
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-07-04 17:55:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,34 @@ var vea='ヴェ';
 var update=function()
 {
 	vea+='ア';
-	document.clear();
-	document.write(vea);
+	
+	document.getElementById('veaaa').textContent=vea;
 	refresh();
 }
+
 
 var refresh=function()
 {
 	setTimeout(update,1000);
 }
 
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+var filename = getFileName();
+var opt;
+if(filename === 'other.html')
+{
+  opt = document.querySelector('option[value="other.html"]');
+}
+else
+{
+ opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
+
+document.getElementById('form').select.onchange=function(){
+  location.href = document.getElementById('form').select.value
+}
+
+update();
