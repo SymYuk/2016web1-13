2049b81b703381986cebe183692961ef3a5868f2
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-07-04 18:01:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var vea='ヴェ';
 
 var update=function()
@@ -7,7 +8,24 @@ var update=function()
 	document.getElementById('veaaa').textContent=vea;
 	refresh();
 }
+*/
+var separate_time =function(time)
+{
+  var sec = time.getSeconds();
+  var min = time.getMinutes();
+  var hours = time.getHours();
+  var days = time.getDate();
+  var month = time.getMonth();
+  var year = time.getFullYear();
+  return[sec,min,hours,days,month,year]
+}
+var update =function()
+{
+var now = new Date();
+var counter = separate_time(now)
 
+document.getElementById('countdown').textContent=
+counter[5]+'年'+counter[4]+'月'+counter[3]+'日'+counter[2]+'時'+counter[1]+'分'+counter[0]+'秒';
 
 var refresh=function()
 {
